d30a91a2dc44acbfc12572d29673a9fe023831dc
halg830 <halg830@gmail.com>
2024-01-25 16:12:24 -0500

    administrar presupuesto

---
+++ src/components/administrarPresupuesto.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <main>
+    <article>
+      
+    </article>
+  </main>
+</template>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 import App from './App.vue'
 
-axios.defaults.baseURL = 'http://localhost:4500/api/'
-// axios.defaults.baseURL = 'https://stocktrack-backend.onrender.com/api/'
+// axios.defaults.baseURL = 'http://localhost:4500/api/'
+axios.defaults.baseURL = 'https://stocktrack-backend.onrender.com/api/'
 axios.defaults.headers.common['x-token'] = obtenerToken()
 
 function obtenerToken(){
